4234e70af30a37db45d723c02dbe1e013b04342f
Nadenov Artur <arturad20@gmail.com>
2024-02-26 17:58:51 +0300

    Add Sum All and replaqce equal sign

--- CoolCalcs/CoolCalcs.cpp
+++ CoolCalcs/CoolCalcs.cpp
@@ -1,19 +1,21 @@
 ﻿// CoolCalcs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+using namespace std;
 #include <iostream>
 
 int main()
 {
     float a, b;
-    std::cout << "Enter two numbers: ";
-    std::cin >> a >> b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
 
-    std::cout << "Sum: " << a + b;
+    cout << "Sum = " << a + b << endl;
 
-    float d;
-    std::cout << "Enter two numbers: ";
-    std::cin >> d;
+    float d, g;
+    cout << "Enter two numbers: ";
+    cin >> d >> g;
 
-    std::cout << "Sum2: " << a + b + d;
+    cout << "Sum 2 = " << d + g << endl;
+    cout << "Sum All = " << a + b + d + g << endl;
 }
